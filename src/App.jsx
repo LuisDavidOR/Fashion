8ea56cbf11aca59ad99dc833ba973f2b66c44475
LuisDavidOR <luisdavidoporta777@gmail.com>
2026-03-19 15:15:25 -0600

    Primer commit corregido

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+
+  return(
+    <>
+      <h1>Componente Principal de Fashion</h1>
+    </>
+  )
+}
+
+export default App;
